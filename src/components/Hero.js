e4caeef70d191cb2d4d30aa24b26c13d0df131bf
Jason Thompson <jason@jasont.us>
2021-04-01 14:29:11 -0400

    replace hero cta with scroll-down arrow

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,22 +25,16 @@ const Hero = ({ isDesktop }) => {
             {subtitle || "I'm the Unknown Developer."}
           </h1>
         </Fade>
-        <Fade
-          left={isDesktop}
-          bottom={!isDesktop}
-          duration={1000}
-          delay={1000}
-          distance='30px'
-        >
-          <p className='hero-cta'>
-            <span className='cta-btn cta-btn--hero'>
-              <Link to='about' smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
       </Container>
+      <Fade bottom={true} duration={800} delay={1000} distance='30px'>
+        <Link to='about' smooth duration={1000}>
+          <div class='box'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </Link>
+      </Fade>
     </section>
   );
 };
