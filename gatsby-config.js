b8405525ea6de72d049f3ae99235d79910dfc263
Jason Thompson <jason@jasont.us>
2021-04-05 18:52:48 -0400

    add favicon / fixes for deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,33 @@
 module.exports = {
   siteMetadata: {
-    title: "Portfolio",
+    title: 'Portfolio',
   },
   plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-netlify-cms',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: 'images',
+        path: './src/images/',
       },
-      __key: "images",
+      __key: 'images',
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Jason Thompson | Portfolio`,
+        short_name: `Jason Thompson`,
+        start_url: `/`,
+        background_color: `#1b1b1b`,
+        theme_color: `#02aab0`,
+        display: `standalone`,
+        icon: 'src/images/favicon.png',
+      },
     },
   ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import '../style/index.scss';
 import App from '../components/App';
 
-const portfolio = () => <App />;
+const portfolio = () => {
+  return (
+    <>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Jason Thompson | Portfolio</title>
+        <html lang='en' />
+        <meta
+          name='description'
+          content='Personal Web Developer Portfolio.'
+        />
+      </Helmet>
+      <App />;
+    </>
+  );
+};
 
 export default portfolio;
