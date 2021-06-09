45a6dc803bbebbeee0f672257488536042800ef9
Jason Thompson <jason@jasont.us>
2021-06-09 16:50:25 -0400

    deploy google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import ReactGA from 'react-ga';
 import '../style/index.scss';
 import App from '../components/App';
+
+ReactGA.initialize(process.env.UA);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 const portfolio = () => {
   return (
@@ -10,10 +14,7 @@ const portfolio = () => {
         <meta charSet='utf-8' />
         <title>Jason Thompson | Portfolio</title>
         <html lang='en' />
-        <meta
-          name='description'
-          content='Personal Web Developer Portfolio.'
-        />
+        <meta name='description' content='Personal Web Developer Portfolio.' />
       </Helmet>
       <App />;
     </>
